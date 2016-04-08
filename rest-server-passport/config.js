95b5de319e41f52e3895e2a3d4191268ec95e55f
Ythalo Rossy Saldanha Lira <ythalorossy@gmail.com>
2016-04-07 22:07:40 -0300

    Add passport-facebook

--- rest-server-passport/config.js
+++ rest-server-passport/config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientID: '219639945091846',
+        clientSecret: 'b6e42c8e6f9a59ab4ce9e1c34df941a0',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
 }
